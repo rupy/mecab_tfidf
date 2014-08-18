327da8bde6d9ea9fafcbacf3083c05ac89c67d68
rupy <rupyapps@gmail.com>
2014-08-18 19:18:39 +0900

    青空文庫からtfidfを計算するプログラム

---
+++ aozora.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import MeCab
+import sys
+import string
+import os
+import codecs
+from tfidf import WordCount
+from prettyprint import pp, pp_str
+
+
+wc = WordCount()
+documents = {}
+for folder in os.listdir("aozora/"):
+    if folder != "夏目漱石":
+        continue
+    for file in os.listdir("aozora/" + folder):
+        path = "aozora/" + folder + "/" + file
+        print path
+        # print os.path.exists(path)
+
+        documents[file] = "\n".join([line.encode('utf-8') for line in codecs.open(path, 'r', 'shift_jis')])
+
+
+wc.calc_tfidf(documents)
+#pp(wc.tf)
+# pp(wc.df)
+for k, v in sorted(wc.tf['こころ.txt'].items(), key=lambda x:x[1]):
+    print k, v
+
