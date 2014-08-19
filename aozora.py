9130afd1b20880ae271d2445d468fb1138bf1467
rupy <rupyapps@gmail.com>
2014-08-19 23:53:08 +0900

    結果表示をtfからtfidfに変更

--- aozora.py
+++ aozora.py
@@ -22,10 +22,10 @@ for folder in os.listdir("aozora/"):
 
         documents[file] = "\n".join([line.encode('utf-8') for line in codecs.open(path, 'r', 'shift_jis')])
 
-
+# exit()
 wc.calc_tfidf(documents)
 #pp(wc.tf)
 # pp(wc.df)
-for k, v in sorted(wc.tf['こころ.txt'].items(), key=lambda x:x[1]):
+for k, v in sorted(wc.tfidf['こころ.txt'].items(), key=lambda x:x[1]):
     print k, v
 
